40e91709e0067dfb4ba41b49ddd47d3ea19c6de3
enric meinhardt <enric.meinhardt@ens-paris-saclay.fr>
2024-06-17 15:28:54 +0200

    first commit (no doc, no fit)

---
+++ stab/stab.h
@@ -0,0 +1,6 @@
+double random_uniform(void);
+double random_normal(void);
+double random_stable(double, double);
+void random_uniform_fill(double *, int);
+void random_normal_fill(double *, int);
+void random_stable_fill(double *, int, double, double);
